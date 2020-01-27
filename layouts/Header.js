7165c662015716b52eceb4bbdbe61a24e22028c0
Joshua Soileau <joshua.soileau@gmail.com>
2020-01-27 07:55:27 -0500

    Remove name from header

--- layouts/Header.js
+++ layouts/Header.js
@@ -8,20 +8,6 @@ const Header = () => {
     <header>
       <Link href="/">
         <a className="logo">
-          {/* <svg width="40" height="40" xmlns="http://www.w3.org/2000/svg">
-            <g fill="none" fillRule="evenodd">
-              <path
-                d="M0 0h40v9.6H24.8v17.2c0 8.8-4.133 13.2-12.4 13.2S0 35.6 0 26.8V20h9.6v6.8c-.08 2.4.834 3.6 2.74 3.6 1.907 0 2.86-1.2 2.86-3.6V9.6H0V0z"
-                fillOpacity=".429"
-                fill="#1E58FF"
-              />
-              <path
-                d="M27.6 0C35.867 0 40 4.4 40 13.2V20h-9.6v-6.8c0-2.4-.933-3.6-2.8-3.6-2.8 0-2.8 2.092-2.8 3.602V26.8c0 8.8-4.133 13.2-12.4 13.2S0 35.6 0 26.8V20h9.6v6.8c-.08 2.4.834 3.6 2.74 3.6 1.907 0 2.86-1.2 2.86-3.6V13.2C15.2 4.4 19.333 0 27.6 0z"
-                fillOpacity=".415"
-                fill="#C316FF"
-              />
-            </g>
-          </svg> */}
           <svg width="40" height="40" xmlns="http://www.w3.org/2000/svg">
             <g fill="none" fillRule="evenodd">
               <path
@@ -36,7 +22,6 @@ const Header = () => {
               />
             </g>
           </svg>
-          <span className="logo__text">Joshua Soileau</span>
         </a>
       </Link>
       <Navigation />
